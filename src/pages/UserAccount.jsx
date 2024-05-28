1c4a3dea3aadc8c392c4961138540ebea114eeb3
AsilbekMelikov <melikovasilbek04@gmail.com>
2024-05-28 18:26:43 +0500

    Add the loading shimmer to the user account and finalize the project

--- src/pages/UserAccount.jsx
+++ src/pages/UserAccount.jsx
@@ -1,9 +1,16 @@
-import { Avatar, Box, Divider, Grid, Typography } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Divider,
+  Grid,
+  Skeleton,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import { useGetLoginDataQuery } from "../features/authApiSlice";
 
 const UserAccount = () => {
-  const { data: loginData } = useGetLoginDataQuery();
+  const { data: loginData, isLoading } = useGetLoginDataQuery();
 
   return (
     <Box
@@ -14,58 +21,157 @@ const UserAccount = () => {
         borderRadius: "10px",
       }}
     >
-      <Box sx={{ display: "flex", alignItems: "center", marginBottom: "20px" }}>
-        <Avatar
-          sx={{
-            bgcolor: "#fa7c54",
-            width: "60px",
-            height: "60px",
-            marginRight: "18px",
-          }}
-        >
-          {loginData?.data?.name.slice(0, 2).toUpperCase()}
-        </Avatar>
-        <Box sx={{ display: "flex", flexDirection: "column" }}>
-          <Typography>{loginData?.data?.name}</Typography>
-          <Typography>{loginData?.data?.email}</Typography>
-        </Box>
-      </Box>
-      <Divider sx={{ marginBottom: "25px" }} />
-      <Grid container spacing={3}>
-        <Grid item xs={6} sx={{ paddingY: "25px" }}>
-          Name:
-        </Grid>
-        <Grid item xs={6}>
-          {loginData?.data?.name}
-        </Grid>
-      </Grid>
-      <Divider sx={{ marginBottom: "25px" }} />
-      <Grid container spacing={3}>
-        <Grid item xs={6} sx={{ paddingY: "25px" }}>
-          Email:
-        </Grid>
-        <Grid item xs={6}>
-          {loginData?.data?.email}
-        </Grid>
-      </Grid>
-      <Divider sx={{ marginBottom: "25px" }} />
-      <Grid container spacing={3}>
-        <Grid item xs={6} sx={{ paddingY: "25px" }}>
-          Key:
-        </Grid>
-        <Grid item xs={6}>
-          {loginData?.data?.key}
-        </Grid>
-      </Grid>
-      <Divider sx={{ marginBottom: "25px" }} />
-      <Grid container spacing={3}>
-        <Grid item xs={6} sx={{ paddingY: "25px" }}>
-          Secret:
-        </Grid>
-        <Grid item xs={6}>
-          {loginData?.data?.secret}
-        </Grid>
-      </Grid>
+      {isLoading ? (
+        <>
+          <Box
+            sx={{ display: "flex", alignItems: "center", marginBottom: "20px" }}
+          >
+            <Skeleton
+              variant="circular"
+              width={60}
+              height={60}
+              sx={{ marginRight: "18px" }}
+            />
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <Skeleton variant="text" width={120} sx={{ fontSize: "1rem" }} />
+              <Skeleton variant="text" width={120} sx={{ fontSize: "1rem" }} />
+            </Box>
+          </Box>
+          <Divider sx={{ marginBottom: "25px" }} />
+          <Grid container spacing={3}>
+            <Grid item xs={6} sx={{ paddingY: "25px" }}>
+              <Skeleton
+                variant="text"
+                width={400}
+                height={25}
+                sx={{ fontSize: "1rem" }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <Skeleton
+                variant="text"
+                width={400}
+                height={25}
+                sx={{ fontSize: "1rem" }}
+              />
+            </Grid>
+          </Grid>
+          <Divider sx={{ marginBottom: "25px" }} />
+          <Grid container spacing={3}>
+            <Grid item xs={6} sx={{ paddingY: "25px" }}>
+              <Skeleton
+                variant="text"
+                width={400}
+                height={25}
+                sx={{ fontSize: "1rem" }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <Skeleton
+                variant="text"
+                width={400}
+                height={25}
+                sx={{ fontSize: "1rem" }}
+              />
+            </Grid>
+          </Grid>
+          <Divider sx={{ marginBottom: "25px" }} />
+          <Grid container spacing={3}>
+            <Grid item xs={6} sx={{ paddingY: "25px" }}>
+              <Skeleton
+                variant="text"
+                width={400}
+                height={25}
+                sx={{ fontSize: "1rem" }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <Skeleton
+                variant="text"
+                width={400}
+                height={25}
+                sx={{ fontSize: "1rem" }}
+              />
+            </Grid>
+          </Grid>
+          <Divider sx={{ marginBottom: "25px" }} />
+          <Grid container spacing={3}>
+            <Grid item xs={6} sx={{ paddingY: "25px" }}>
+              <Skeleton
+                variant="text"
+                width={400}
+                height={25}
+                sx={{ fontSize: "1rem" }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <Skeleton
+                variant="text"
+                width={400}
+                height={25}
+                sx={{ fontSize: "1rem" }}
+              />
+            </Grid>
+          </Grid>
+        </>
+      ) : (
+        <>
+          <Box
+            sx={{ display: "flex", alignItems: "center", marginBottom: "20px" }}
+          >
+            <Avatar
+              sx={{
+                bgcolor: "#fa7c54",
+                width: "60px",
+                height: "60px",
+                marginRight: "18px",
+              }}
+            >
+              {loginData?.data?.name.slice(0, 2).toUpperCase()}
+            </Avatar>
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <Typography>{loginData?.data?.name}</Typography>
+              <Typography>{loginData?.data?.email}</Typography>
+            </Box>
+          </Box>
+          <Divider sx={{ marginBottom: "25px" }} />
+          <Grid container spacing={3}>
+            <Grid item xs={6} sx={{ paddingY: "25px" }}>
+              Name:
+            </Grid>
+            <Grid item xs={6}>
+              {loginData?.data?.name}
+            </Grid>
+          </Grid>
+          <Divider sx={{ marginBottom: "25px" }} />
+          <Grid container spacing={3}>
+            <Grid item xs={6} sx={{ paddingY: "25px" }}>
+              Email:
+            </Grid>
+            <Grid item xs={6}>
+              {loginData?.data?.email}
+            </Grid>
+          </Grid>
+          <Divider sx={{ marginBottom: "25px" }} />
+          <Grid container spacing={3}>
+            <Grid item xs={6} sx={{ paddingY: "25px" }}>
+              Key:
+            </Grid>
+            <Grid item xs={6}>
+              {loginData?.data?.key}
+            </Grid>
+          </Grid>
+          <Divider sx={{ marginBottom: "25px" }} />
+          <Grid container spacing={3}>
+            <Grid item xs={6} sx={{ paddingY: "25px" }}>
+              Secret:
+            </Grid>
+            <Grid item xs={6}>
+              {loginData?.data?.secret}
+            </Grid>
+          </Grid>
+        </>
+      )}
     </Box>
   );
 };
