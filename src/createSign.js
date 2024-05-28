c0a6a638480c2c41f3523f21fc956db93ee8779f
AsilbekMelikov <melikovasilbek04@gmail.com>
2024-05-28 17:58:55 +0500

    Change the path of crypto sign file

--- src/createSign.js
+++
@@ -1,7 +0,0 @@
-export const cryptoSign = (method, path, body = "") => {
-  const crypto = require("crypto-js");
-  const secret = localStorage.getItem("Secret");
-  let signStr = `${method}${path}${body}${secret}`;
-  let sign = crypto.MD5(signStr).toString();
-  return sign;
-};
