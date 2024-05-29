4638f6952eb7db0725d4546232f9cd3c5668340e
AsilbekMelikov <melikovasilbek04@gmail.com>
2024-05-29 16:40:16 +0500

    Add dark mode component

---
+++ src/components/DarkMode.jsx
@@ -0,0 +1,29 @@
+import { useTheme } from "@emotion/react";
+import { IconButton } from "@mui/material";
+import React, { createContext, useContext } from "react";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+
+export const ColorModeContext = createContext({
+  toggleColorMode: () => {},
+});
+
+const DarkMode = ({ marginLeft }) => {
+  const theme = useTheme();
+  const colorMode = useContext(ColorModeContext);
+  return (
+    <IconButton
+      sx={{ ml: marginLeft }}
+      onClick={colorMode.toggleColorMode}
+      color="inherit"
+    >
+      {theme.palette.mode === "dark" ? (
+        <Brightness7Icon />
+      ) : (
+        <Brightness4Icon />
+      )}
+    </IconButton>
+  );
+};
+
+export default DarkMode;
